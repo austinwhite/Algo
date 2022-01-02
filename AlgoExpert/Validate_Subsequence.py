2c143611fb7d2ef7e5c3e1a5369f60f44e7ff628
austinwhite <austinlldb@gmail.com>
2022-01-01 17:23:36 -0700

    validate subsequence

---
+++ AlgoExpert/Validate_Subsequence.py
@@ -0,0 +1,13 @@
+# Time Complexty: Linear, O(n) where n is the length of array
+# Space Complexty: Constant, 0(1)
+
+def isValidSubsequence(array, sequence):
+	index = 0
+	
+	for value in array:
+		if index == len(sequence):
+			break
+		if value == sequence[index]:
+			index += 1
+			
+	return index == len(sequence)
