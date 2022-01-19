775cde183193a53ea08c30461cfe66c9dd6fa475
Austin White <austinlldb@gmail.com>
2022-01-18 18:00:58 -0700

    depth first search

---
+++ AlgoExpert/Depth_First_Search.py
@@ -0,0 +1,18 @@
+# Time: O(v+e) where v is the number of nodes 
+#       and e is the depth of the tree
+# Space: O(v) where ve is the number of nodes
+
+class Node:
+    def __init__(self, name):
+        self.children = []
+        self.name = name
+
+    def addChild(self, name):
+        self.children.append(Node(name))
+        return self
+
+    def depthFirstSearch(self, array):
+        array.append(self.name)
+        for child in self.children:
+            child.depthFirstSearch(array)
+        return array
