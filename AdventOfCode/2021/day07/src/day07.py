10c2da0828330ad8fdc0e29e25a4959d6ff7b45e
Austin White <austinlldb@gmail.com>
2022-02-06 23:58:10 -0700

    day07 part2 naive solution

--- AdventOfCode/2021/day07/src/day07.py
+++ AdventOfCode/2021/day07/src/day07.py
@@ -6,13 +6,33 @@ positions = []
 with open(infile) as f:
     positions = [int(x) for x in f.readline().strip().split(',')]
 
-def part1():
-    med = int(median(positions))
-
+def calculate_fuel(position):
     total_fuel = 0
     for num in positions:
-        total_fuel += abs(med - num)
+        total_fuel += abs(position - num)
     
     return total_fuel
 
+def natural_number_sum(position):
+    return (position * (position + 1)) // 2
+
+def part1():
+    median_position = int(median(positions))
+    return calculate_fuel(median_position)
+    
+def part2():
+    min_fuel = 1 << 100
+    min_position = min(positions)
+    max_position = max(positions)
+
+    for position in range(min_position, max_position):
+        fuel = 0
+        for crab in positions:
+            fuel += natural_number_sum(abs(crab - position))
+        
+        min_fuel = min(min_fuel, fuel)
+
+    return min_fuel
+
 print("Part 1:", part1())
+print("Part 2:", part2())
