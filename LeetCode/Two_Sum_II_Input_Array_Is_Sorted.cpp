84b5cb2c2733399509216c136a40d98bebb96558
Austin White <austinlldb@gmail.com>
2022-02-13 18:50:10 -0700

    two sum II

--- LeetCode/Two_Sum_II_Input_Array_Is_Sorted.cpp
+++ LeetCode/Two_Sum_II_Input_Array_Is_Sorted.cpp
@@ -5,21 +5,19 @@ class Solution {
 public:
     vector<int> twoSum(vector<int>& numbers, int target) {
         int left = 0;
-        int right = numbers.size()-1;
+        int right = numbers.size() - 1;
         vector<int> match;
-        
+
         while (left < right) {
-            int test = numbers[left] + numbers[right];
-            
-            if (test == target) {
-                break;
-            } else if (test < target) {
-                left++;
+            int value = numbers[left] + numbers[right];
+            if (value < target) {
+               left += 1; 
+            } else if (value > target) {
+                right -= 1;
             } else {
-                right--;
+                break;
             }
         }
-        
         match.push_back(left+1);
         match.push_back(right+1);
         return match;
