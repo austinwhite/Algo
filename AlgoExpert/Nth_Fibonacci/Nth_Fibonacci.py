d512731c51a24e5d0556560bd91e5f05c4d5b6bd
Austin White <austinlldb@gmail.com>
2022-02-21 12:41:29 -0700

    nth fibonacci -- memoization solution

--- AlgoExpert/Nth_Fibonacci/Nth_Fibonacci.py
+++ AlgoExpert/Nth_Fibonacci/Nth_Fibonacci.py
@@ -8,3 +8,16 @@ def getNthFib(n):
         return 1
     else:
         return getNthFib(n-1) + getNthFib(n-2)
+
+# Time: O(n)
+# Space: O(n)
+
+def getNthFib(n):
+    memo = {1: 0, 2: 1}
+    return getNthFibHelper(n, memo)
+
+def getNthFibHelper(n, memo):
+    if n not in memo:
+        nth_value = getNthFibHelper(n-1, memo) + getNthFibHelper(n-2, memo)
+        memo[n] = nth_value
+    return memo[n]
