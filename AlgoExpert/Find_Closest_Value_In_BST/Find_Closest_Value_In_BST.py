988d5dff144e5cb075c2a2a9f698e1c4ef5344da
Austin White <austinlldb@gmail.com>
2022-03-09 23:09:03 -0700

    find closest value in bst

---
+++ AlgoExpert/Find_Closest_Value_In_BST/Find_Closest_Value_In_BST.py
@@ -0,0 +1,16 @@
+# Time: Avg: O(log(n)), worst: O(n)
+# Space: Avg: O(log(n)), worst: O(n)
+def findClosestValueInBst(tree, target):
+    return findClosestValueInBstHelper(tree, target, tree.value)
+
+def findClosestValueInBstHelper(tree, target, closest):
+    if tree is None:
+        return closest
+    if abs(target - closest) > abs(target - tree.value):
+        closest = tree.value
+    if target < tree.value:
+        return findClosestValueInBstHelper(tree.left, target, closest)
+    elif target > tree.value:
+        return findClosestValueInBstHelper(tree.right, target, closest)
+    else:
+        return closest
