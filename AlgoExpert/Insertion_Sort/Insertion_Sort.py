e0bba314cccfc5a5fe128ecf1a448186b80694ee
Austin White <austinlldb@gmail.com>
2022-03-25 19:59:34 -0600

    insertion sort

---
+++ AlgoExpert/Insertion_Sort/Insertion_Sort.py
@@ -0,0 +1,12 @@
+# Best: Time: O(n), Space: O(1)
+# Average/Worst: Time: O(n^2), Space: O(n^2)
+def insertionSort(array):
+    for i in range(1, len(array)):
+		j = i
+		while j > 0 and array[j] < array[j-1]:
+			swap(j, j-1, array)
+			j -= 1
+	return array
+
+def swap(i, j, array):
+	array[i], array[j] = array[j], array[i]
