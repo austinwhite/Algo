58e0ec280d6835d7bc11e991877e80bc188a8d04
Austin White <austinlldb@gmail.com>
2022-03-27 14:28:08 -0600

    selection sort

--- AlgoExpert/Selection_Sort/Selection_Sort.py
+++ AlgoExpert/Selection_Sort/Selection_Sort.py
@@ -1,15 +1,14 @@
 # Time: Best/Average/Worst: O(n^2)
 # Space: Best/Avererage/Worst: O(n^2)
 def selectionSort(array):
-    curr = 0
-    while curr < len(array) - 1:
-        smallest = curr
-        for i in range(curr + 1, len(array)):
-            if array[smallest] > array[i]:
-                smallest = i
-        swap(curr, smallest, array)
-        curr += 1
-    return array
+	for i in range(len(array)):
+		curr = i
+		smallest = i
+		for j in range(i, len(array)):
+			if array[j] < array[smallest]:
+				smallest = j
+		swap(curr, smallest, array)
+	return array
 
 def swap(i, j, array):
-    array[i], array[j] = array[j], array[i]
+	array[i], array[j] = array[j], array[i]
